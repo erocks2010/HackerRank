f4053d2eeb4a51bdbc86cfb21e55a2d70573ba99
TM48854 <tarun.mathur@derivco.co.sa>
2017-11-21 15:21:44 +0530

    solved few easy ones.

---
+++ Strings/camelcase.js
@@ -0,0 +1,30 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = readLine();
+    var count=1;
+    for(let i of s){
+        if (i.charCodeAt(0)>=65 && i.charCodeAt(0) <=90) count++
+    }
+    console.log(count);
+}
